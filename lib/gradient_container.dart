12b1d62ac9aebb52bddc2185a0963d22ae2cbbe6
Kyaw Myo Htet <kyawmyohtet.uit@gmail.com>
2024-10-27 16:02:42 +0700

    dice roller app

---
+++ lib/gradient_container.dart
@@ -0,0 +1,25 @@
+import 'package:first_flutter_app/dice_roller.dart';
+import 'package:flutter/material.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(this.colors, {super.key});
+
+  final List<Color> colors;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: colors,
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        // child: StyledText('Display Image here'),
+        child: DiceRoller(),
+      ),
+    );
+  }
+}
